2e1c2c93539420fd818f0dc3a3cdc4436b06cec6
MiniOcean404 <1037306928@qq.com>
2023-12-30 13:25:02 +0800

    feat:  整理文件

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-import { start } from "./lib/main.js";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-start(process.argv[2]);
+import { genREADME } from "./lib/main.js";
+import { genRepList, genTodoList } from "./lib/utils/list.js";
+import { genLine } from "./lib/utils/line.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const root = process.env();
+
+genREADME(process.argv[2]);
+
+// 生成 README 文件内容
+export function genREADME(token) {
+  // 复制模版内容到 README
+  fs.copyFileSync(path.join(root, "lib/template/md/index.md"), path.join(root, "README.md"));
+
+  // 把 github-status 内容追加到 README
+  const githubStatusContent = fs.readFileSync(path.join(root, "lib/template/md/github-info.md"), "utf-8");
+  appendToReadme(githubStatusContent);
+
+  fs.appendFileSync("./README.md", githubStatusContent);
+
+  // genRepList();
+  // genLine();
+  // genTodoList();
+}
